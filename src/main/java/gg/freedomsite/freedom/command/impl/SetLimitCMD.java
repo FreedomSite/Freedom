af5d52e587e28f91857d5b7007e45afa17122ff9
vJayyy <hentscheljr1@aol.com>
2020-08-21 22:07:37 -0400

    Increase limit for VPS

--- src/main/java/gg/freedomsite/freedom/command/impl/SetLimitCMD.java
+++ src/main/java/gg/freedomsite/freedom/command/impl/SetLimitCMD.java
@@ -19,19 +19,19 @@ public class SetLimitCMD extends FreedomCommand
     public void run(CommandSender sender, String[] args)
     {
         Bukkit.getOnlinePlayers().forEach(p -> {
-            getPlugin().getWorldEditBridge().setLimit(p, 8000);
+            getPlugin().getWorldEditBridge().setLimit(p, 20000);
         });
 
         if (!(sender instanceof Player))
         {
             if (sender.getName().equalsIgnoreCase("CONSOLE"))
             {
-                bcastMsg("§cFreedom - Setting default WorldEdit limit to 8000");
+                bcastMsg("§cFreedom - Setting default WorldEdit limit to 20k blocks");
             } else {
-                bcastMsg("§c" + sender.getName() + " - Setting default WorldEdit limit to 8000");
+                bcastMsg("§c" + sender.getName() + " - Setting default WorldEdit limit to 20k blocks");
             }
         } else {
-            bcastMsg("§c" + sender.getName() + " - Setting default WorldEdit limit to 8000");
+            bcastMsg("§c" + sender.getName() + " - Setting default WorldEdit limit to 20k blocks");
         }
 
 
