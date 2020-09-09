34cee110ee8f24f320ddd7b1a26c9f54f49412d0
spacerocket62 <spacerocket62@gmail.com>
2020-09-09 09:06:32 -0700

    update dm listener

--- src/main/java/gg/freedomsite/freedom/discord/listeners/DMListener.java
+++ src/main/java/gg/freedomsite/freedom/discord/listeners/DMListener.java
@@ -3,7 +3,9 @@ package gg.freedomsite.freedom.discord.listeners;
 import gg.freedomsite.freedom.Freedom;
 import gg.freedomsite.freedom.cache.DiscordCache;
 import gg.freedomsite.freedom.player.FPlayer;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.apache.commons.lang.math.NumberUtils;
@@ -13,13 +15,17 @@ public class DMListener extends ListenerAdapter
 {
 
     @Override
-    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event)
+    public void onMessageReceived(MessageReceivedEvent event)
     {
         String msg = event.getMessage().getContentRaw();
         Member member = event.getMessage().getMember();
         assert member != null;
+        if (event.getChannelType() != ChannelType.PRIVATE) return;
+        if (member.getIdLong() == Freedom.get().getDiscordBot().getBot().getSelfUser().getIdLong()) return;
         if (member.getUser().isBot()) return;
         if (!NumberUtils.isNumber(msg)) return;
+
+        member.getUser().openPrivateChannel().queue();
 
         long code = Long.parseLong(msg);
         if (DiscordCache.isValidCode(code)) {
